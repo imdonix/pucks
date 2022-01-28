74e758375893096a1cf7d30570a06d15801ca2a5
imdonix <tamas.donix@gmail.com>
2022-01-28 18:53:39 +0100

    Added base

---
+++ Assets/Code/MonoSingleton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public abstract class MonoSingleton<T> : MonoBehaviour
+{
+    public static T Instance { private set; get; }
+
+    private void Awake()
+    {
+        if (ReferenceEquals(Instance, null))
+        {
+            MonoSingleton<T>.Instance = gameObject.GetComponent<T>();
+        }
+    }
+}
